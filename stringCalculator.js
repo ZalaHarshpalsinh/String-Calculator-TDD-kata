97d0ee1f1eed6269e4665b5d3727b4e225fdb772
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2025-07-04 09:26:43 +0530

    Refactored the code to pass the failing test: Handles the newline between numbers as a delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,8 +4,11 @@ function add( numbers )
         // sum for empty string is 0
         if ( numbers === '' ) return 0;
 
-        // parse all the numbers in the string
-        const parsedNumbers = numbers.split( ',' ).map( ( num ) => parseInt( num ) )
+        //define regex for supported default delimiters
+        const delimiter = /,|\n/g
+
+        // split all the numbers in the string using delimiter and parse each of them to convert to a number
+        const parsedNumbers = numbers.split( delimiter ).map( ( num ) => parseInt( num ) )
 
         // sum all the parsed numbers
         return parsedNumbers.reduce( ( sum, number ) => sum += number )
